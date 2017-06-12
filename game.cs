ab5d355106bb22535d0b5a4c6d2ac17ece18a8d3
James Hosken <jameswfhosken@gmail.com>
2017-06-12 18:06:15 -0400

    responded to Pierre's super helpful feedback about Start

--- game.cs
+++ game.cs
@@ -1,8 +1,19 @@
 public void Start(){
 	
+	//Helpful feedback:
 	Debug.Log("there is an extra line carriage above me");
 }
 
 public void Update(){
+
 	Debug.Log("FRAME");
+
+	int data = Input.GetAxisRaw("Horizontal");
+	//Interaction
+	Interaction(data);
+
+}
+
+public void Interaction(int data){
+	//create awesome stuff
 }
